182838720d77492f50fd061438ddf1c5be891732
kimjy0730 <kimjy2767@naver.com>
2025-01-14 19:57:05 +0900

    Update Character.h

--- Adventure/Character.h
+++ Adventure/Character.h
@@ -9,14 +9,14 @@ class Character
 {
 private:
 
-	static Character* instance;		// 珥湲고 .cpp 댁＜湲
+	static Character* instance;
 	string name;
 	int level;
 	int health;
 	int maxHealth;
 	int attack;
-	int remainingExperience;		// ⑥ 寃쏀移
-	int maxExperience;				// 理 寃쏀移
+	int remainingExperience;
+	int maxExperience;
 	int experience;
 	int gold;
 	vector<Item*> inventory;
@@ -28,17 +28,16 @@ public:
 	void displayStatus();
 	void levelUp();
 	void useRandomItem();		
-	void enhanceAttack(int attackIncrease);		// 怨듦꺽�μ 뱀ㅺ린  ⑥
-	void Heal(int heal);						//  泥대 蹂
-	void takeDamage(int damage);				// 怨듦꺽 諛 
+	void enhanceAttack(int attackIncrease);
+	void Heal(int heal);
+	void takeDamage(int damage);
 	bool isDead();
-	void addExperience(int exp);				// 寃쏀移 	
-	void addGold(int _gold);					// 怨⑤ 
-	void getDropedItem(Item* item);				// 紐ъㅽ 泥移   댄 
+	void addExperience(int exp);
+	void addGold(int _gold);
+	void getDropedItem(Item* item);
 	void showInventory();
 	vector<Item*>& getInventory();
-	void sortInventoryByName();																//댄 ��
-
+	void sortInventoryByName();
 	// get/set 없이 하는 방법이 있나? 일단 만들어보고 다 함수로 만드는 거 생각해보자
 	// 몬스터의 체력, 공격력에 레벨이 필요
 	// 어떤 행동에 대한 처리를 여기서 해버리면 됨(takeDamage, addExperience 등)
