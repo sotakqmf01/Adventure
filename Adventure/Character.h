99927576f180450fa2f076593cf0fb1cd90d2bd9
kimjy0730 <kimjy2767@naver.com>
2025-01-14 20:12:54 +0900

    bug

--- Adventure/Character.h
+++ Adventure/Character.h
@@ -9,35 +9,35 @@ class Character
 {
 private:
 
-	static Character* instance;
+	static Character* instance;			// 초기화 .cpp에서 해주기
 	string name;
 	int level;
-	int health;
+	int health;  
 	int maxHealth;
 	int attack;
-	int remainingExperience;
-	int maxExperience;
+	int remainingExperience;			// 남은 경험치
+	int maxExperience;					// 최대 경험치
 	int experience;
 	int gold;
 	vector<Item*> inventory;
-
+private:
+	Character(const string& name);
 
 public:
-
 	static Character* getInstance(const string& name);
 	void displayStatus();
 	void levelUp();
 	void useRandomItem();		
-	void enhanceAttack(int attackIncrease);
-	void Heal(int heal);
-	void takeDamage(int damage);
+	void enhanceAttack(int attackIncrease);		// 공격력을 상승시키기 위한 함수
+	void Heal(int heal);						// 현재 체력 회복
+	void takeDamage(int damage);				// 공격을 받았을 때
 	bool isDead();
-	void addExperience(int exp);
-	void addGold(int _gold);
-	void getDropedItem(Item* item);
+	void addExperience(int exp);				// 경험치 획득
+	void addGold(int _gold);					// 골드 획득
+	void getDropedItem(Item* item);				// 몬스터 처치 후 드랍된 아이템 획득
 	void showInventory();
 	vector<Item*>& getInventory();
-	void sortInventoryByName();
+	void sortInventoryByName();					//아이탬 정렬
 	// get/set 없이 하는 방법이 있나? 일단 만들어보고 다 함수로 만드는 거 생각해보자
 	// 몬스터의 체력, 공격력에 레벨이 필요
 	// 어떤 행동에 대한 처리를 여기서 해버리면 됨(takeDamage, addExperience 등)
