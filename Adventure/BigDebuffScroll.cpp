cf5a940ebd7a11d2e95918400bedcd1917f74e24
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-16 02:50:17 +0900

    fix : ignore strange input fix : modify game closing

--- Adventure/BigDebuffScroll.cpp
+++ Adventure/BigDebuffScroll.cpp
@@ -1,10 +1,9 @@
-#include <iostream>
+﻿#include <iostream>
 #include "AttackBoost.h"
 #include "BigDebuffScroll.h"
 using namespace std;
 
 
 BigDebuffScroll::BigDebuffScroll() : AttackBoost("대형 디버프 스크롤", -125, -20)
-
 {
 }
