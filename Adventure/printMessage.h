af55b687661e37ecdf0319c1a6dcdcb4dbb6f13c
정재훈 <57822478+sotakqmf01@users.noreply.github.com>
2025-01-16 12:36:18 +0900

    del : printCongratulations()

--- Adventure/printMessage.h
+++ Adventure/printMessage.h
@@ -13,7 +13,6 @@ public:
 	void printIntro();
 	void bossAppears();
 	void displayEpitaph(string name);
-	void printCongratulations();
 	void gotoXY(int X, int Y);				// 메세지 출력 위치 지정을 위한 커서 위치 지정 함수
 	int getcursorlocationY();				// 프레임 출력 후 같은 줄에 내용 출력을 위해 커서 좌표 따오는 함수
 	void askName();							// 캐릭터 생성과정 1. 이름 입력
